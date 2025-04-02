7e5693dfb560e607a1309ad2bf262034a4ac59e8
Jeyaruban jenushan <jjenushan550@gmail.com>
2025-04-02 19:09:08 +0530

    backend application update

---
+++ appointmentbackend/src/main/java/org/ai/appointmentbackend/AppointmentbackendApplication.java
@@ -0,0 +1,13 @@
+package org.ai.appointmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentbackendApplication.class, args);
+	}
+
+}
